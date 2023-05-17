a0f19442cf69b43781d6a9afa649716ca54d0e62
BradyFoster <Bjfoster9860@gmail.com>
2023-05-17 14:10:23 -0700

    Complete functions continued

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+const mondayWork = function (work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+mondayWork();
+momndayWork("I will work from home")
+
+function wrapAdjective(style = "*") {
+    return function(adjective = "special") {
+        return `You are ${style}${adjective}${style}!`
+    }
+
+}
